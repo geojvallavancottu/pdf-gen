34964f50091488b27c4f77a3388b0de37bc7d47d
Geo J Vallavancottu <geoj3030@gmail.com>
2023-02-08 23:34:52 +0530

    uuid

--- index.js
+++ index.js
@@ -3,71 +3,37 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const puppeteer = require('puppeteer');
-
+const fs = require('fs');
+const uuid = require('uuid/v4');
 
 var html_to_pdf = require('html-pdf-node');
-let options = { timeout: 300000, 
-//     margin: {
-//     top:'0.4cm',
-//     right:'0.39cm',
-//     bottom:'0.49cm',
-//     left:'0.4cm'
-// }
- };
+let options = {
+    timeout: 300000,
+    format: 'A4',
+};
 app.use(express.json({ limit: '150mb' }));
-
-app.post('/pdf', async (req, res) => {
-    const browser = await puppeteer.launch({ timeout: 300000 });
-    const page = await browser.newPage();
-    let html = req.body.body;
-    console.log(html);
-    await page.setContent(html);
-    const buffer = await page.pdf({ format: 'A4' });
-    const filePath = path.resolve(__dirname, 'pdfs', 'dynamic.pdf');
-
-    if (!fs.existsSync('pdfs')) {
-        fs.mkdirSync('pdfs');
-    }
-
-    fs.writeFileSync(filePath, buffer);
-
-    res.send({ filePath });
-
-    await browser.close();
-});
 
 app.post('/generate-pdf', (req, res) => {
     let html = req.body.body;
     console.log(html);
+    let filename = uuid()+'.pdf';
+    console.log(filename);
     let file = { content: html };
     html_to_pdf.generatePdf(file, options).then(pdfBuffer => {
         console.log("PDF Buffer:-", pdfBuffer);
-        let pth = require('fs').writeFileSync('./pdf/example.pdf', pdfBuffer);
-        // console.log(pth)
-        // res.setHeader('Content-Type', 'application/pdf');
-        // res.setHeader('Content-Length', pdfBuffer.length);
-        // return res.send({ pdf: pdfBuffer }).json();
+        const filePath = path.resolve(__dirname, 'pdf', filename);
+        if (!fs.existsSync('pdf')) {
+            fs.mkdirSync('pdf');
+        }
+        fs.writeFileSync(filePath, pdfBuffer);
+        console.log(filePath);
     });
-    // html_to_pdf.generatePdf(html).then(pdfBuffer => {
-    //     res.setHeader('Content-Type', 'application/pdf');
-    //     res.setHeader('Content-Length', pdfBuffer.length);
-    //     res.send(pdfBuffer);
-    // });
     return res.status(200).json()
-    // const fileName = 'output.pdf';
-    // const filePath = path.join(__dirname, 'pdf', fileName);
-
-    // pdf.create(html).toFile(filePath, (err, result) => {
-    //     if (err) return res.send(err);
-
-    //     res.send({ filePath: result.filename });
-    // });
 });
 
 app.post('/receive', (req, res) => {
     console.log("post");
     console.log(req.body.body);
-    // return "HELLO";
     return res.status(200).json()
 });
 
